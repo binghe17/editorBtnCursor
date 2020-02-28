a89a537d88c7be342171ece2fdb761e1f6163bb0
zun <binghe17@qq.com>
2020-02-28 18:33:48 +0900

    test

--- index.js
+++ index.js
@@ -25,22 +25,80 @@ ui.insImage.addEventListener('click', function(e){
 	// let img = new Image();
 	// img.src= '/favicon.ico';
 
-	let img = document.createElement('img');
-	img.setAttribute('src', 'https://cdn.icon-icons.com/icons2/2151/PNG/128/map_map_marker_icon_132682.png');
+	// let img = document.createElement('img');
+	// img.setAttribute('src', 'https://cdn.icon-icons.com/icons2/2151/PNG/128/map_map_marker_icon_132682.png');
+	// // SM.insertElement(img);
+	// range.deleteContents();
+	// range.insertNode(img);
+	// range.collapse(false);
 
-	// SM.insertElement(img);
-	range.deleteContents();
-	range.insertNode(img);
-	range.collapse(false);
-	
+
+	// let div = document.createElement('span');
+	// // div.setAttribute('contenteditable','false');
+	// div.className = 'imageWrapper';
+	// div.innerHTML = '<img src="https://cdn.icon-icons.com/icons2/2151/PNG/128/map_map_marker_icon_132682.png">';
+	// range.deleteContents();
+	// range.insertNode(div);
+	// range.collapse(false);
+
+
+	// let code = `<br><img src="https://cdn.icon-icons.com/icons2/2151/PNG/128/map_map_marker_icon_132682.png"><br>`;
+	let code = `<div class="imageWrapper" contenteditable="false"><img src="https://cdn.icon-icons.com/icons2/2151/PNG/128/map_map_marker_icon_132682.png"></div>`;
+	//jquery사용
+	$(code).each(function(i,elem){
+		// SM.insertElement(elem);
+		range.deleteContents();
+		range.insertNode(elem);
+		range.collapse(false);
+	});
 	ui.editor.focus();
+	// console.log(focusDom())
+	// showElementCursor(focusDom())
 });
+
+
+
+
+
+// function createCaretPlacer(atStart) {
+//     return function(el) {
+//         el.focus();
+//         if (typeof window.getSelection != "undefined"  && typeof document.createRange != "undefined") {
+// 			console.log('1111111')
+//             var range = document.createRange();
+//             range.selectNodeContents(el);
+//             range.collapse(atStart);
+//             var sel = window.getSelection();
+//             sel.removeAllRanges();
+//             sel.addRange(range);
+//         } else if (typeof document.body.createTextRange != "undefined") {
+// 			console.log('2222222')
+//             var textRange = document.body.createTextRange();
+//             textRange.moveToElementText(el);
+//             textRange.collapse(atStart);
+//             textRange.select();
+//         }
+//     };
+// }
+
+// var placeCaretAtStart = createCaretPlacer(true);
+// var placeCaretAtEnd = createCaretPlacer(false);
+
+// editor = document.querySelector('#editor');
+// if (editor.exportSelection() == null) {
+//     var placeCaretAtEnd = createCaretPlacer(false);
+//     placeCaretAtEnd(document.getElementById(id));
+// }
+// editor.pasteHTML('<div>1111111</div>');
+
+
 
 ui.insImage2.addEventListener('click', function(e){
 	ui.editor.focus();
 
 	// let code = `<img src="https://cdn.icon-icons.com/icons2/2151/PNG/128/propellers_icon_132704.png"><div>DIV</div>`;
-	let code = `<div><p>IFRAME유튜브</p><iframe width="560" height="315" src="https://www.youtube.com/embed/80za9zdZKzk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`;
+	// let code = `<div><p>IFRAME유튜브</p><iframe width="560" height="315" src="https://www.youtube.com/embed/80za9zdZKzk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`;
+	let code = `<div class="iframeWrapper" contenteditable="false"><div><iframe width="560" height="315" src="https://www.youtube.com/embed/80za9zdZKzk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div></div><br>`;
 	//jquery사용
 	$(code).each(function(i,elem){
 		// SM.insertElement(elem);
@@ -49,90 +107,135 @@ ui.insImage2.addEventListener('click', function(e){
 		range.collapse(false);
 	})
 	ui.editor.focus();
+
+	// var placeCaretAtEnd = createCaretPlacer(false);
+	// placeCaretAtEnd(this);
+	// console.log(event.target)
+
+
+
 });
 
 
 
-// $("#editor").on("keydown",function(e){if(e.which == 13) { e.preventDefault(); pasteHtmlAtCaret("<br/>");}});
-// $('div[contenteditable]').keydown(function(e) {
-//     // trap the return key being pressed
-//     if (e.keyCode === 13) {
-//         // insert 2 br tags (if only one br tag is inserted the cursor won't go to the next line)
-//         document.execCommand('insertHTML', false, '<br/>');
-//         // prevent the default behaviour of return key pressed
-//         return false;
-//     }
-// });
+
 
 //Enter키 입력시 div,p태그 내에서 다 br태그 생성
 // $('div[contenteditable="true"]').keypress(function(event) {
 // 	if (event.which != 13) return true;
-
 // 	var docFragment = document.createDocumentFragment();
-// 	//add a new line
-// 	var newEle = document.createTextNode('\n');
+// 	var newEle = document.createTextNode('\n');	//add a new line
 // 	docFragment.appendChild(newEle);
-
-// 	//add the br, or p, or something else
-// 	newEle = document.createElement('br');
+// 	newEle = document.createElement('br');//add the br, or p, or something else
 // 	docFragment.appendChild(newEle);
-
-// 	//make the br replace selection
-// 	var range = window.getSelection().getRangeAt(0);
+// 	var range = window.getSelection().getRangeAt(0);//make the br replace selection
 // 	range.deleteContents();
 // 	range.insertNode(docFragment);
-
-// 	//create a new range
-// 	range = document.createRange();
+// 	range = document.createRange();//create a new range
 // 	range.setStartAfter(newEle);
 // 	range.collapse(true);
-
-// 	//make the cursor there
-// 	var sel = window.getSelection();
+// 	var sel = window.getSelection();//make the cursor there
 // 	sel.removeAllRanges();
 // 	sel.addRange(range);
-
 // 	return false;
 // });
 
+//--------enter br
+var div=document.querySelector('[contenteditable="true"]');
+div.onkeypress=function(e){
+	if( e.keyCode==13 ){
+		var selection=window.getSelection(),
+		range=selection.getRangeAt(0),
+		br=document.createElement("br");
+		range.deleteContents();
+		range.insertNode(br);
+		range.setStartAfter(br);
+		range.setEndAfter(br);
+		range.collapse(false);
+		selection.removeAllRanges();
+		selection.addRange(range);
+		return false;
+	}
+};
+div.onkeyup=function(){
+  var a=document.activeElement;
+  if( a.lastChild && a.lastChild.nodeName!="BR" ){
+    a.appendChild(document.createElement("br"));
+  }
+};
 
-//---------------------------------
+
+
+
+
+
+//---------------------------------이벤트
 
 let cursorPoint;
+//저장버튼  
 ui.saveCursorPoint.addEventListener('click', function(e){//保存焦点1 及 失去焦点
 	cursorPoint = window.getSelection().getRangeAt(0);
 	console.log('save',cursorPoint)
 });
+//복구버튼
 ui.loadCursorPoint.addEventListener('click', function(e){//还原焦点1
 	console.log('load',cursorPoint)
-	cursorPoint.setStart(cursorPoint.startContainer, cursorPoint.startOffset);
-	cursorPoint.setEnd(cursorPoint.endContainer, cursorPoint.endOffset);
-	sel = window.getSelection()
-	sel.removeAllRanges();
-	sel.addRange(cursorPoint);
-
+	if(cursorPoint !== undefined){
+		cursorPoint.setStart(cursorPoint.startContainer, cursorPoint.startOffset);
+		cursorPoint.setEnd(cursorPoint.endContainer, cursorPoint.endOffset);
+		sel = window.getSelection()
+		sel.removeAllRanges();
+		sel.addRange(cursorPoint);
+	}
 });
-
-$('#editor *').click(function(){
+//에디터의 모든 요소를 클릭할떄
+$('#editor').on('click','*',function(){
 	event.stopPropagation();
-	document.querySelector('#domPathBox').innerHTML= getDomPath(this).join(' > ');
-	// console.log(this)
-	if(isAccessTagName(this))
-		showElementCursor(tagNameSelectDom(this));//this.parentNode
-		
+	console.log(this, this.parentNode)
+	if(this.className.indexOf('imageWrapper') > -1){//이미지 영역을 클릭시
+		console.log(showElementCursor(focusDom()))
+	}else if(this.tagName == 'IMG'){
+		console.log(111111, this.parentNode, showElementCursor(this.parentNode))
+
+	}
+	else{
+		document.querySelector('#domPathBox').innerHTML= getDomPath(this).join(' > ');
+		if(isAccessTagName(this)){
+			showElementCursor(tagNameSelectDom(this));//this.parentNode
+		}
+	}
 })
+
+//버튼
+$('#left').click(function(){
+	console.log(focusDom())
+	// console.log(rangeText())
+	// console.log(rangeTextLength())
+	
+})
+$('#center').click(function(){
+	console.log(focusDom());
+})
+
+
+
+
+
 
 
 
 
 //===============================================================
 
-//판단; 태그이름에 속할때
+//판단; 태그이름에 속할때만 실행용
 function isAccessTagName(elem, accessTagName='|IMG|IFRAME|A|'){
 	if(accessTagName.indexOf('|'+ elem.tagName +'|') > -1) return true;
 	else return false;
 }
+//태그넴에 속한자의 부모를 반환
 function tagNameSelectDom(elem, accessTagName='|IMG|IFRAME|'){
+	// console.log(elem)
+	//contenteditable=false일떄 안에 img가 선택안 되고 상위의 span이 선택된다. 
 	if(accessTagName.indexOf('|'+ elem.tagName +'|') > -1) return elem.parentNode;
 	else return elem;
 }
@@ -146,15 +249,52 @@ function showElementCursor(elem, isInFront=2){
 	range.selectNode(elem);
 	if(isInFront === 1) range.collapse(true);
 	else if(isInFront === 2) range.collapse(false);
-	console.log(this)
+	elem.focus();
+	// console.log(this)
 }
 
-//커서위치가 변할때
+
+
+//커서위치에 있는 dom 반환
+function focusDom(type=1){
+	range = window.getSelection().getRangeAt(0);
+	if(type === 1) return range.endContainer;
+	else return range.startContainer;
+
+	//선택한 DOM내용바꾸기
+	// if(range.endContainer.nodeType === 1){//1일때 DOM
+	// 	range.endContainer.innerHTML =555
+	// }else {//3일때 TEXT
+	// 	range.endContainer.nodeValue = 333
+	// }
+}
+
+//선택한 텍스트
+function rangeText(){
+	range = window.getSelection().getRangeAt(0);
+	return range.toString();
+}
+//선택한 텍스트의 글자수
+function rangeTextLength(){
+	range = window.getSelection().getRangeAt(0);
+	return range.toString().length;
+}
+
+//앞으로 몇번째인지 넣으면 뒤로 몇번째인지 계산해준다.
+function getTextEndIndex(str, startIndex){
+	return - str.slice(startIndex).length ;
+}
+// console.log(getTextEndIndex('asdfasdf', 3))
 
 
 
 
-// ------------------------------
+
+
+
+
+
+// =====================================================
 //dom path구하기.
 function getDomPath(el) {
 	var stack = [];
